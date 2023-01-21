d0833d4fc7916541ce737d9a702a3a9cbcfdffc3
Dmitrii126776 <dukek67@gmail.com>
2023-01-20 22:25:44 -0500

    create tile calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,207 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Col, Form, FormGroup, Input, Label, Row} from "reactstrap";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [areaWidth, setAreaWidth] = useState(0)
+    const [areaHeight, setAreaHeight] = useState(0)
+    const [areaPercent, setAreaPercent] = useState(10)
+    const [resultArea, setResultArea] = useState(0)
+
+    const [tilesWidth, setTilesWidth] = useState(0)
+    const [tilesHeight, setTilesHeight] = useState(0)
+    const [resultAmount, setResultAmount] = useState(0)
+
+    const [select, setSelect] = useState('')
+    const [pricePiece, setPricePiece] = useState(0)
+    const [totalPricePiece, setTotalPricePiece] = useState('0')
+
+    const [priceBox, setPriceBox] = useState(0)
+    const [tilesInBox, setTilesInBox] = useState(0)
+    const [boxQuantity, setBoxQuantity] = useState(0)
+    const [totalPriceBox, setTotalPriceBox] = useState('0')
+
+    const [modal, setModal] = useState(false)
+
+    const toggle = () => {
+        setModal(!modal)
+    }
+
+    const calculateArea = () => {
+        const initialArea = (areaWidth * areaHeight) * 144
+        const percentArea = initialArea / 100 * areaPercent
+        setResultArea(Math.ceil(initialArea + percentArea))
+    }
+    const calculateAmount = () => {
+        const tileSquare = tilesWidth * tilesHeight
+        setResultAmount(Math.ceil(resultArea / tileSquare))
+    }
+
+    const calculateBox = () => {
+        setBoxQuantity(Math.ceil(resultAmount / tilesInBox))
+    }
+
+    const calculateTotalPricePiece = () => {
+        if (select === 'Piece') {
+            const total = resultAmount * pricePiece
+            setTotalPricePiece(total.toFixed(2))
+        } else {
+            setPricePiece(0)
+            setTotalPricePiece('0')
+        }
+    }
+
+    const calculateTotalPriceBox = () => {
+        if (select === 'Box') {
+            const total = boxQuantity * priceBox
+            setTotalPriceBox(total.toFixed(2))
+        } else {
+            setPriceBox(0)
+            setTotalPriceBox('0')
+            setTilesInBox(0)
+        }
+    }
+
+    useEffect(() => {
+        calculateArea();
+        calculateAmount();
+        calculateTotalPricePiece();
+        calculateBox()
+        calculateTotalPriceBox()
+    }, [areaWidth, areaHeight, areaPercent, resultArea, tilesWidth, tilesHeight,
+        resultAmount, select, pricePiece, totalPricePiece, boxQuantity, tilesInBox, totalPriceBox]);
+
+    return (
+        <div className="App">
+            <h1>Tiles calculator</h1>
+            <Form>
+                <Row className="row-cols-lg-auto g-3 align-items-center">
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Area Width, ft </Label>
+                            <Input min='0' type="number" value={areaWidth} onChange={e => {
+                                setAreaWidth(e.target.value)
+                                calculateArea()
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Area Height, ft </Label>
+                            <Input min='0' type="number" value={areaHeight} onChange={e => {
+                                setAreaHeight(e.target.value)
+                                calculateArea()
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Area Addition Percent </Label>
+                            <Input min='0' type="number" value={areaPercent} onChange={e => {
+                                setAreaPercent(e.target.value)
+                                calculateArea()
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Form>
+
+            <Form>
+                <Row className="row-cols-lg-auto g-3 align-items-center">
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Tiles Width, ft </Label>
+                            <Input min='0' type="number"
+                                   value={tilesWidth} onChange={e => {
+                                setTilesWidth(e.target.value)
+                                calculateAmount()
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Tiles Height, ft </Label>
+                            <Input min='0' type="number"
+                                   value={tilesHeight} onChange={e => {
+                                setTilesHeight(e.target.value)
+                                calculateAmount()
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                    <Col xs='4'>
+                        <FormGroup>
+                            <Label> Sell Options </Label>
+                            <Input value={select} onChange={e => {
+                                setSelect(e.target.value)
+                                toggle()
+                            }}
+                                   name="select" type="select">
+                                <option>Piece</option>
+                                <option>Box</option>
+                            </Input>
+                        </FormGroup>
+                    </Col>
+                    {!modal &&
+                        <Col xs='4'>
+                            <FormGroup>
+                                <Label>Price per piece</Label>
+                                <Input value={pricePiece} onChange={e => {
+                                    setPricePiece(e.target.value)
+                                    calculateTotalPricePiece()
+                                }}
+                                       min='0' type="number"/>
+                            </FormGroup>
+                        </Col>
+                    }
+                    {modal &&
+                        <Col xs='4'>
+                            <FormGroup>
+                                <Label>Price per box</Label>
+                                <Input value={priceBox} onChange={e => {
+                                    setPriceBox(e.target.value)
+                                    calculateTotalPriceBox()
+                                }}
+                                       min='0' type="number"/>
+                            </FormGroup>
+                        </Col>
+                    }
+                </Row>
+                {modal &&
+                    <Row className="row-cols-lg-auto g-3 align-items-center">
+                        <Col xs='4'>
+                            <FormGroup>
+                                <Label>Tiles in Box </Label>
+                                <Input value={tilesInBox} onChange={e => {
+                                    setTilesInBox(e.target.value)
+                                    calculateBox()
+                                    calculateTotalPriceBox()
+                                }}
+                                       min='0' type="number"/>
+
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                }
+            </Form>
+            {!modal &&
+                <>
+                    <h5>Area: {resultArea} sq in</h5>
+                    <h5>Tiles Amount: {resultAmount}</h5>
+                    <h5>Total Price: {totalPricePiece}</h5>
+                </>
+            }
+            {modal &&
+                <>
+                    <h5>Area: {resultArea} sq in</h5>
+                    <h5>Tiles Amount: {resultAmount}</h5>
+                    <h5>Box Quantity: {boxQuantity}</h5>
+                    <h5>Total Price: {totalPriceBox}</h5>
+                </>
+            }
+        </div>
+    );
 }
 
 export default App;
